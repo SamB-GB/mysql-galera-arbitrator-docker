4ad7c4bb7c182ed05451f29ce337c36352971b32
Kirill <kirill@indigit.info>
2023-05-04 14:11:13 +0200

    add env for passing additional options

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 /usr/bin/garbd \
     --address "gcomm://${MARIADB_ADDRESSES}" \
     --group "${MARIADB_GALERA_CLUSTER_NAME}" \
-    --options "pc.bootstrap=FALSE;"
+    --options "pc.bootstrap=FALSE;${PROVIDER_OPTIONS}"

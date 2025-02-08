cbe54508b6b5467a852fbcdca31d09deca56d1f7
Samuel <samuel.barr@outlook.com>
2025-02-08 03:03:19 +0000

    Updated Variables

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 /usr/bin/garbd \
-    --address "gcomm://${MARIADB_ADDRESSES}" \
-    --group "${MARIADB_GALERA_CLUSTER_NAME}" \
+    --address "gcomm://${MYSQL_ADDRESSES}" \
+    --group "${GALERA_CLUSTER_NAME}" \
     --options "pc.bootstrap=FALSE;${PROVIDER_OPTIONS}"
